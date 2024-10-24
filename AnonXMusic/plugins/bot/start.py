8be67f4563896a48d809e5bffb0ae3674d1f7eeb
Ronin <bhabyaprakash01@gmail.com>
2024-10-25 00:13:26 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -88,7 +88,7 @@ async def start_pm(client, message: Message, _):
             f"๏ ᴀ ʟᴀᴛᴇꜱᴛ ᴄᴏɴᴛᴇɴᴛ ꜱᴀᴠɪɴɢ ʙᴏᴛ ꜰᴏʀ ʏᴏᴜ.\n"
             f"๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs</b>"
         )
-        await message.reply(f"{message_text} \n<a href='{config.START_VIDEO}'> </a>", reply_markup=InlineKeyboardMarkup(out))
+        await message.reply(f"{message_text} \n<a href='{config.START_VIDEO}'>.</a>", reply_markup=InlineKeyboardMarkup(out))
 
         if await is_on_off(2):
             return await app.send_message(
