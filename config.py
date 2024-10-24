d256b2849c800b95b6c8325614f7988a5e32d17d
Ronin <bhabyaprakash01@gmail.com>
2024-10-24 22:41:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = 20948356
 API_HASH = "6b202043d2b3c4db3f4ebefb06f2df12"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "6994525123:AAGIubqHaPE2gaaUDC-vtMWJMtpSKSENsWI"
+BOT_TOKEN = "7416862801:AAFDMC3LWqlZTh_Nxs8KPvO4ONYRUIYforQ"
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = "mongodb+srv://ComboBot:ComboBot@combobot.ek5lw.mongodb.net/?retryWrites=true&w=majority&appName=ComboBot"
