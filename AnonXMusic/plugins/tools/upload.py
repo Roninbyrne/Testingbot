767695f98458b001dfe91563e077b33de0f98afe
Ronin <bhabyaprakash01@gmail.com>
2024-10-24 21:04:23 +0530

    Update upload.py

--- AnonXMusic/plugins/tools/upload.py
+++ AnonXMusic/plugins/tools/upload.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from AnonXMusic import app
+from anonXMusic import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pymongo import MongoClient
 from datetime import datetime, timedelta
