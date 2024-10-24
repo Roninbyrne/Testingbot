f6050c9d6958b7bdfaedb4415d4732219ead33b5
Ronin <bhabyaprakash01@gmail.com>
2024-10-24 23:02:51 +0530

    Update ping.py

--- AnonXMusic/plugins/tools/ping.py
+++ AnonXMusic/plugins/tools/ping.py
@@ -15,19 +15,18 @@ async def ping(client, message):
         uptime = str(timedelta(seconds=int(uptime_seconds)))
 
         ram = psutil.virtual_memory()
-        ram_usage = f"{ram.percent}% used ({ram.used / (1024 ** 3):.2f} GB of {ram.total / (1024 ** 3):.2f} GB)"
+        ram_usage = f"{ram.percent}% ᴜꜱᴇᴅ ({ram.used / (1024 ** 3):.2f} ɢʙ ᴏꜰ {ram.total / (1024 ** 3):.2f} ɢʙ)"
         cpu_usage = f"{psutil.cpu_percent(interval=1)}%"
         disk = psutil.disk_usage('/')
-        disk_usage = f"{disk.percent}% used ({disk.used / (1024 ** 3):.2f} GB of {disk.total / (1024 ** 3):.2f} GB)"
+        disk_usage = f"{disk.percent}% ᴜꜱᴇᴅ ({disk.used / (1024 ** 3):.2f} ɢʙ ᴏꜰ {disk.total / (1024 ** 3):.2f} ɢʙ)"
 
         # Merge the image and stats into one message
         stats_message = (
             f"{client.mention} sʏsᴛᴇᴍ sᴛᴀᴛs : <a href='{PING_IMG_URL}'>.</a>\n\n"
-            f"↬ sʏsᴛᴇᴍ sᴛᴀᴛs\n\n"
             f"↬ ᴜᴩᴛɪᴍᴇ : {uptime}\n"
-            f"↬ ʀᴀᴍ : {ram_usage}\n"
             f"↬ ᴄᴩᴜ : {cpu_usage}\n"
-            f"↬ ᴅɪsᴋ : {disk_usage}\n"
+            f"↬ ʀᴀᴍ : {ram_usage}\n"
+            f"↬ ᴅɪsᴋ : {disk_usage}\n\n"
         )
 
         await message.reply(stats_message)
