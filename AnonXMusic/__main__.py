81230be547ef72f5551f464d2d61bc77003cf301
Ronin <bhabyaprakash01@gmail.com>
2024-10-24 23:21:43 +0530

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -1,8 +1,10 @@
 import asyncio
 import importlib
-import config 
 
 from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
 from AnonXMusic import LOGGER, app
 from AnonXMusic.misc import sudo
 from AnonXMusic.plugins import ALL_MODULES
@@ -11,15 +13,6 @@ from config import BANNED_USERS
 
 
 async def init():
-    if (
-        not config.STRING1
-        and not config.STRING2
-        and not config.STRING3
-        and not config.STRING4
-        and not config.STRING5
-    ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
-        exit()
     await sudo()
     try:
         users = await get_gbanned()
@@ -34,7 +27,18 @@ async def init():
     for all_module in ALL_MODULES:
         importlib.import_module("AnonXMusic.plugins" + all_module)
     LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
-    
+    await userbot.start()
+    await Anony.start()
+    try:
+        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("AnonXMusic").error(
+            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Anony.decorators()
     LOGGER("AnonXMusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
