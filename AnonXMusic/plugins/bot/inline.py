e98482cedbebfb8eb2c038acdc2eb3c9bfc9d40d
Ronin <bhabyaprakash01@gmail.com>
2024-10-25 13:04:06 +0530

    Update inline.py

--- AnonXMusic/plugins/bot/inline.py
+++ AnonXMusic/plugins/bot/inline.py
@@ -5,9 +5,8 @@ from pyrogram.types import (
 )
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.utils.inlinequery
-import answer
-from AnonXMusic import app
+from AnonXMusic import app
+from AnonXMusic.utils.inlinequery import answer
 from config import BANNED_USERS
 
 
