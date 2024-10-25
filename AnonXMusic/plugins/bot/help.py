7dea91d774eba629dee62e6b3df07499dc6e0468
Ronin <bhabyaprakash01@gmail.com>
2024-10-25 12:48:40 +0530

    Update help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -42,14 +42,7 @@ async def helper_private(
             photo=START_IMG_URL,
             caption=_["help_1"].format(SUPPORT_CHAT),
             reply_markup=keyboard,
-        )
-
-
-@app.on_message(filters.command(["help"]) & filters.group & ~BANNED_USERS)
-@LanguageStart
-async def help_com_group(client, message: Message, _):
-    keyboard = private_help_panel(_)
-    await message.reply_text(_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard))
+        )
 
 
 @app.on_callback_query(filters.regex("help_callback") & ~BANNED_USERS)
